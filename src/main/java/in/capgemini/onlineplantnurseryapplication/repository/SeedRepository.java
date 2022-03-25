a6321634705106a52ee36ae89b1f04efc0685934
ritik1022 <55057989+ritik1022@users.noreply.github.com>
2022-03-25 09:02:20 +0530

    Create SeedRepository.java

---
+++ src/main/java/in/capgemini/onlineplantnurseryapplication/repository/SeedRepository.java
@@ -0,0 +1,9 @@
+package in.capgemini.onlineplantnurseryonline.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import in.capgemini.onlineplantnurseryonline.models.Seed;
+
+public interface SeedRepository extends CrudRepository<Seed, Integer> {
+
+}
